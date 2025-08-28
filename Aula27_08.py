5091ee87963d724b8c8c197c43c9c3dcb314976e
EA - Alice Maria Da Silva Salmazi <alice_salmazi@educadventista.org>
2025-08-27 22:44:44 -0300

    Segundo commit

--- Aula27_08.py
+++ Aula27_08.py
@@ -1,5 +1,7 @@
-# Aula 27.08
+# Aula 27.08 - Revisão
+import os
 
+# For
 familia = {"Pai": "Fábio",
            "Mãe" : "Cristiene",
            "Filhos": ["Alice", "Lucas", "Sofia", "João Vitor"]}
@@ -9,8 +11,27 @@ for key, valor in familia.items():
         print("Filhos:")
         for i, nome in enumerate(valor):
             if i == (len(valor) - 1):
-                print(f"- {nome}.")
+                print(f"- {nome}.\n")
             else:
                 print(f"- {nome};")
     else:
         print(f"{key}: {valor};")
+
+os.system('pause')
+os.system('cls')
+
+# While
+import random
+
+lista = []
+for _ in range(10):
+    lista.append(random.randint(1, 10))
+
+indice = 0
+print("Lista Aleatória (8 itens):")
+while indice < 8:
+    if indice == 7:
+        print(f"- {lista[indice]}.")
+    else:
+        print(f"- {lista[indice]};")
+    indice += 1
